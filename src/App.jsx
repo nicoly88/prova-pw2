e5a69e6359ed8b1bd35a06b015696ca74f3b7f0b
nicoly88 <nicoly2005correa@estudante.ifro.edu.br>
2022-06-24 18:13:11 -0400

    adicionando arquivos para prova

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,57 @@
+import './App.css'
+
 function App() {
     return (
-        <div>
-            Hello World
+        <div class = "container">
+
+        <div class = "medio">
+        <button> Processos </button>
+        <br></br>
+        <p>Processos relacionados ao combate do COVID-19 </p>
+        </div>
+
+        <div class = "medio">
+        <button>Contratos</button>
+        <br></br>
+        <p>Contrato relacionados ao combate do COVID-19</p>
+        </div>
+
+        <div class = "grande">
+        <button1> Licitações e Compras Diretas </button1>
+        <br></br>
+        <p>Compras destinadas ao enfrentamento e combate a COVID-19</p>
+        </div>
+
+        <div class = "medio">
+        <button2> Receitas Federais </button2>
+        <br></br>
+        <p>Receitas federais destinadas ao enfrentamento e combate a COVID-19</p>
+        </div>
+
+        <div class = "medio">
+        <button2> Receitas Estaduais </button2>
+        <br></br>
+        <p>Receitas estaduais destinadas ao enfrentamento e combate a COVID-19</p>
+        </div>
+
+        <div class = "pequeno">
+        <button3> Empenhos </button3>
+        <br></br>
+        <p>Despesas empenhadas destinadas ao enfrentamento e combate a COVID-19</p>
+        </div>
+
+        <div class = "pequeno">
+        <button3> Liquidações </button3>
+        <br></br>
+        <p>Despesas liquidas destinadas ao enfrentamento e combate a COVID-19</p>
+        </div>
+
+        <div class = "pequeno">
+        <button3> Pagamentos </button3>
+        <br></br>
+        <p>Despesas pagas destinadas ao enfrentamento e combate a COVID-19</p>
+        </div>
+
         </div>
     )
 }
